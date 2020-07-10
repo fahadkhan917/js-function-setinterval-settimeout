387f3264979db6b7f63b62d85f675b5385c7b250
fahadkhan917 <fk16181@gmail.com>
2020-07-10 23:07:12 +1000

    update

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 
 
+// var count=5;
+
+// function time(){
+
+// count++;
+// document.write(count);
+
+// }
+
+
+
+// setInterval(time,1000);
+
+
+// function time(){
+
+// document.write("fahad");
+
+// }
+
+// setTimeout(time,1000)
+
 var count=0;
 var interval;
-function timer(){
+function time(){
 
     count++;
     console.log(count);
@@ -10,8 +32,7 @@ function timer(){
 
 }
 
-interval=setInterval(timer,1000);
+interval=setInterval(time,1000);
 
-setTimeout(function(){
-    clearInterval(interval)
-},5000)
+
+setTimeout(function(){clearInterval(interval)},10000)
